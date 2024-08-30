bd8b943c51624ffca2c8d9a92d72374e2c1a09dd
Mahbubur Miah <mahbuburmiah@mahbuburs-imac.mynetworksettings.com>
2024-08-30 19:23:10 -0400

    update

--- HW/hw06.py
+++ HW/hw06.py
@@ -6,3 +6,8 @@ elif temp < 55:
     print("Pleasant")
 elif temp < 73:
     print("Getting Warmer")
+elif temp < 101:
+    print("Very Hot")
+else:
+    print("Please input a valid temperature")
+    
